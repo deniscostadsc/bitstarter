84b38f04be665d4e2aa569b33b4e143fc13539f4
Denis Costa <deniscostadsc@gmail.com>
2013-07-19 20:24:45 -0300

    Adding some CSS

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 
-var express = require('express');
+var express = require('express'), app = express();
 
-var app = express.createServer(express.logger());
+app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', 'utf-8'));
+    response.send(fs.readFileSync('index.html', 'utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
